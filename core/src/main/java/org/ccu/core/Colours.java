1982f2d64deb2516aeeadaf936cca254472ef606
Fesaa <77553571+Fesaa@users.noreply.github.com>
2023-01-17 01:16:22 +0100

    Stats command dipped in new layout - Still have to remake the outputs in the game trackers

--- core/src/main/java/org/ccu/core/Colours.java
+++ core/src/main/java/org/ccu/core/Colours.java
@@ -8,5 +8,6 @@ public class Colours {
   public static NamedTextColor Primary = NamedTextColor.AQUA;
   public static NamedTextColor Secondary = NamedTextColor.WHITE;
   public static NamedTextColor Error = NamedTextColor.RED;
+  public static NamedTextColor Hover = NamedTextColor.GREEN;
 
 }
--- core/src/main/java/org/ccu/core/commands/StatCommands.java
+++ core/src/main/java/org/ccu/core/commands/StatCommands.java
@@ -1,13 +1,20 @@
 package org.ccu.core.commands;
 
 import com.google.inject.Inject;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
 import java.util.regex.Pattern;
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.event.ClickEvent;
+import net.kyori.adventure.text.event.HoverEvent;
 import net.labymod.api.client.chat.command.Command;
 import org.ccu.core.CCU;
+import org.ccu.core.Colours;
 import org.ccu.core.config.CCUManager;
 import org.ccu.core.config.imp.GameStatsTracker;
+import org.ccu.core.config.subconfig.StatsTrackerSubConfig;
 
 public class StatCommands extends Command {
 
@@ -21,68 +28,157 @@ public class StatCommands extends Command {
     this.addon = addon;
   }
 
-  // TODO: Reorder importance of arguments, a specified game should go before the game you're currently in.
-  //  Failing in specifying should fail the command with an error message
   // TODO: Colours
   @Override
   public boolean execute(String prefix, String[] arguments) {
     CCUManager manager = this.addon.getManager();
-
-    if (!manager.onCubeCraft()) {
-      return false;
-    }
-
-    GameStatsTracker gameStatsTracker = this.addon.configuration().getStatsTrackerSubConfig().getGameStatsTrackers().get(manager.getDivisionName());
-
-    if (gameStatsTracker == null) {
-
-      GameStatsTracker gameStatsTrackerFromArguments = this.addon.configuration().getStatsTrackerSubConfig().getGameStatsTrackers().get(String.join(" ", arguments));
-
-      if (gameStatsTrackerFromArguments != null) {
-        this.displayMessage(gameStatsTrackerFromArguments.getDisplayComponent(this.addon));
-        
-        return true;
-      }
-
-      return false;
-    }
+    StatsTrackerSubConfig config = this.addon.configuration().getStatsTrackerSubConfig();
+    HashMap<String, GameStatsTracker> allGameStatsTrackers = config.getGameStatsTrackers();
 
     if (arguments.length == 0) {
-      this.displayMessage(gameStatsTracker.getDisplayComponent(this.addon));
-
-      return true;
-    } else if (arguments.length == 1) {
-
-      if (this.timeFormat.matcher(arguments[0]).matches()) {
-        GameStatsTracker snapShot = gameStatsTracker.getHistoricalData(arguments[0]);
-        this.displayMessage(snapShot.getDisplayComponent(this.addon));
-      } else {
-        this.displayMessage(gameStatsTracker.getUserStatsDisplayComponent(this.addon, arguments[0]));
-      }
-
-      return true;
-    } else if (arguments.length == 2) {
-
-      if (this.timeFormat.matcher(arguments[0]).matches()) {
-        GameStatsTracker snapShot = gameStatsTracker.getHistoricalData(arguments[0]);
-        this.displayMessage(snapShot.getUserStatsDisplayComponent(this.addon, arguments[1]));
-      } else if (this.timeFormat.matcher(arguments[1]).matches()) {
-        GameStatsTracker snapShot = gameStatsTracker.getHistoricalData(arguments[1]);
-        this.displayMessage(snapShot.getUserStatsDisplayComponent(this.addon, arguments[0]));
-      } else {
-        Component errorComponent = Component.empty()
-            .append(this.addon.prefix())
-            .append(Component.text("Unknown request! Layout: ", NamedTextColor.RED))
-            .append(Component.text("\n    •/stats YYYY-MM-DD <user name>", NamedTextColor.GRAY))
-            .append(Component.text("\n    •/stats <user name> YYYY-MM-DD", NamedTextColor.GRAY))
-            .append(Component.text("\n    -Date should not include trailing 0's.", NamedTextColor.GRAY))
-            .append(Component.text("\n    -Ex: 2023-1-1 for First of January 2023.", NamedTextColor.GRAY));
-        this.displayMessage(errorComponent);
-      }
-
-      return true;
-    } else {
       return false;
     }
+
+    String gameName = this.getGameString(arguments);
+
+    GameStatsTracker gameStatsTracker;
+    if (gameName == null ) {
+       gameStatsTracker = allGameStatsTrackers.get(manager.getDivisionName());
+    } else {
+      gameStatsTracker = allGameStatsTrackers.get(gameName);
+    }
+
+    if (gameStatsTracker == null) {
+      if (!arguments[0].equals("help")) {
+        this.displayMessage(
+            this.addon.prefix()
+                .append(Component.text(" Could not find the specified game. Possible options are ", Colours.Error))
+                .append(this.gamesList().color(Colours.Error))
+        );
+        return false;
+      } else {
+        this.helpCommand();
+        return true;
+      }
+    }
+
+    if (gameName != null) {
+      arguments = this.removeGameNameFromArguments(arguments, gameName);
+    }
+
+    switch (arguments.length) {
+      case 0: {
+        this.displayMessage(gameStatsTracker.getDisplayComponent(this.addon));
+        return true;
+      }
+      case 1: {
+        if (this.timeFormat.matcher(arguments[0]).matches()) {
+          GameStatsTracker snapShot = gameStatsTracker.getHistoricalData(arguments[0]);
+          this.displayMessage(snapShot.getDisplayComponent(this.addon));
+        } else if (arguments[0].equals("help")) {
+          this.helpCommand();
+        } else {
+          this.displayMessage(gameStatsTracker.getUserStatsDisplayComponent(this.addon, arguments[0]));
+        }
+        return true;
+      }
+      case 2: {
+        if (this.timeFormat.matcher(arguments[0]).matches()) {
+          GameStatsTracker snapShot = gameStatsTracker.getHistoricalData(arguments[0]);
+          this.displayMessage(snapShot.getUserStatsDisplayComponent(this.addon, arguments[1]));
+          return true;
+        } else if (this.timeFormat.matcher(arguments[1]).matches()) {
+            GameStatsTracker snapShot = gameStatsTracker.getHistoricalData(arguments[1]);
+            this.displayMessage(snapShot.getUserStatsDisplayComponent(this.addon, arguments[0]));
+          return true;
+        } else if (arguments[0].equals("help")) {
+          this.helpCommand();
+        }
+      }
+    }
+
+
+    return false;
+  }
+
+  private void helpCommand() {
+    Component helpComponent = this.addon.prefix()
+        .append(Component.text("------- Enhanced Stats Commands -------", Colours.Title))
+        .append(Component.text("\n/stats <game>", Colours.Primary)
+            .clickEvent(ClickEvent.suggestCommand("/stats ")))
+        .append(Component.text(" Displays your stats in a game.", Colours.Secondary))
+        .append(Component.text("\n/stats <game> <username>", Colours.Primary)
+            .clickEvent(ClickEvent.suggestCommand("/stats ")))
+        .append(Component.text(" Displays your kills on and deaths by a player.", Colours.Secondary))
+        .append(Component.text("\n/stats <game> <YYYY-MM-DD>", Colours.Primary)
+            .clickEvent(ClickEvent.suggestCommand("/stats ")))
+        .append(Component.text(" Displays your stats in a game on a certain day (Omit real 0's).", Colours.Secondary))
+        .append(Component.text("\n/stats <game> <YYYY-MM-DD> <username>", Colours.Primary)
+            .clickEvent(ClickEvent.suggestCommand("/stats "))
+            .hoverEvent(HoverEvent.showText(Component.text("  This has to be enabled in settings.", Colours.Hover))))
+        .append(Component.text(" Displays your stats in a game on a certain day with a player (Omit real 0's).", Colours.Secondary)
+            .hoverEvent(HoverEvent.showText(Component.text("  This has to be enabled in settings.", Colours.Hover))))
+        .append(Component.text("\n/stats help", Colours.Primary)
+            .clickEvent(ClickEvent.suggestCommand("/stats ")))
+        .append(Component.text(" Displays this message.", Colours.Secondary))
+        .append(Component.text("\n\nGames tracked: ", Colours.Primary))
+        .append(this.gamesList().color(Colours.Secondary));
+
+    this.displayMessage(helpComponent);
+  }
+
+  private Component gamesList() {
+    Component comp = Component.empty();
+    Set<String> keySet = this.addon.configuration().getStatsTrackerSubConfig().getGameStatsTrackers().keySet();
+    List<String> keys = new ArrayList<>(keySet);
+    for (int i = 0; i < keys.size(); i++) {
+      String game = keys.get(i);
+      comp = comp
+          .append(Component.text(game)
+              .clickEvent(ClickEvent.suggestCommand("/stats " + game + " ")));
+      if (i != keys.size() - 1) {
+        comp = comp.append(Component.text(", "));
+      }
+    }
+    return comp;
+  }
+
+  private String[] removeGameNameFromArguments(String[] arguments, String gameName) {
+    String[] gameComponents = gameName.split(" ");
+
+    for (String comp : gameComponents) {
+      arguments = this.removeFromArguments(arguments, comp);
+    }
+    return arguments;
+  }
+
+  private String[] removeFromArguments(String[] arguments, String member) {
+    String[] newArguments = new String[arguments.length];
+
+    int j = 0;
+    for (String argument : arguments) {
+      if (!argument.equals(member)) {
+        newArguments[j] = argument;
+        j++;
+      }
+    }
+    return newArguments;
+  }
+
+  private String getGameString(String[] arguments) {
+    String game = "";
+
+    for (String arg : arguments) {
+      game = game + arg;
+
+      GameStatsTracker tracker = this.addon.configuration().getStatsTrackerSubConfig().getGameStatsTrackers()
+          .get(game);
+
+      if (tracker != null) {
+        return game;
+      }
+    }
+
+    return null;
   }
 }
