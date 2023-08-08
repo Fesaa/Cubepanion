4608d3bbe9ff7bdcdb903229878df3e6a1ebba97
Fesaa <77553571+Fesaa@users.noreply.github.com>
2023-08-08 12:56:22 +0200

    Update gradle plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "Cubepanion"
 
 pluginManagement {
-    val labyGradlePluginVersion = "0.3.26"
+    val labyGradlePluginVersion = "0.3.28"
     plugins {
         id("net.labymod.gradle") version (labyGradlePluginVersion)
     }
