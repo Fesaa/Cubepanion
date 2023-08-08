6fc57f79dffe9f0404a4530e8193eeff677c4027
Fesaa <77553571+Fesaa@users.noreply.github.com>
2023-08-08 13:39:06 +0200

    Proper debug logs

--- core/src/main/java/org/cubepanion/core/utils/LOGGER.java
+++ core/src/main/java/org/cubepanion/core/utils/LOGGER.java
@@ -3,22 +3,20 @@ package org.cubepanion.core.utils;
 import net.labymod.api.Laby;
 import net.labymod.api.client.entity.player.ClientPlayer;
 import net.labymod.api.util.logging.Logging;
-import org.cubepanion.core.Cubepanion;
 import org.cubepanion.core.managers.CubepanionManager;
+import org.jetbrains.annotations.NotNull;
 
 public class LOGGER {
 
   private static Logging log = null;
   private static CubepanionManager manager = null;
-  private static boolean enabled = false;
 
-  public static void init() {
-    log = Cubepanion.get().logger();
-    manager = Cubepanion.get().getManager();
+  public static void setLog(Logging logging) {
+    log = logging;
   }
 
-  public static void setEnabled(boolean enabled1) {
-    enabled = enabled1;
+  public static void setManager(CubepanionManager m) {
+    manager = m;
   }
 
   public static void setDebug(boolean debug) {
@@ -29,18 +27,27 @@ public class LOGGER {
     return "[" + origin.getSimpleName() + "@" + origin.getPackageName() + "] ";
   }
 
-  private static String getGameInfo() {
-    String out = "\nDivision: " + manager.getDivision() + ", "
-        + "Map: " + manager.getMapName();
+  private static @NotNull String getGameInfo() {
+    if (manager == null) {
+      return "GameInfo{}";
+    }
+    String out = "\nGameInfo{Division: " + manager.getDivision()
+        + ", Map: " + manager.getMapName()
+        + ", Alive: " + !manager.isEliminated();
+
+    if (manager.getDivision().equals(CubeGame.TEAM_EGGWARS)) {
+      out += ", Team Colour: " + manager.getTeamColour();
+    }
 
     ClientPlayer player = Laby.labyAPI().minecraft().getClientPlayer();
     if (player != null) {
-      out += ", Location: " + player.position()
+      out += ", PlayerInfo{Location: " + player.position()
           + ", MainHand: " + player.getMainHandItemStack()
-          + ", OffHand: " + player.getOffHandItemStack();
+          + ", OffHand: " + player.getOffHandItemStack()
+          + "}";
     }
 
-    return out;
+    return out + "}";
   }
 
   private static <T> String join(T[] msgs) {
@@ -53,86 +60,63 @@ public class LOGGER {
 
   @SafeVarargs
   public static <T, V> void info(Class<T> origin, V... msgs) {
-    if (enabled) {
-      log.info(formatClass(origin) + join(msgs));
-    }
+    log.info(formatClass(origin) + join(msgs));
   }
 
   @SafeVarargs
   public static <T, V> void info(Class<T> origin, Throwable cause, V... msgs) {
-    if (enabled) {
-      log.info(formatClass(origin) + join(msgs), cause);
-    }
+    log.info(formatClass(origin) + join(msgs), cause);
   }
 
   @SafeVarargs
   public static <T, V> void info(boolean gameInfo, Class<T> origin, V... msgs) {
-    if (enabled) {
-      log.info(formatClass(origin) + join(msgs) + (gameInfo ? getGameInfo() : ""));
-    }
+    log.info(formatClass(origin) + join(msgs) + (gameInfo ? getGameInfo() : ""));
   }
 
   @SafeVarargs
   public static <T, V> void warn(Class<T> origin, V... msgs) {
-    if (enabled) {
-      log.warn(formatClass(origin) + join(msgs));
-    }
+    log.warn(formatClass(origin) + join(msgs));
   }
 
   @SafeVarargs
   public static <T, V> void warn(Class<T> origin, Throwable cause, V... msgs) {
-    if (enabled) {
-      log.warn(formatClass(origin) + join(msgs), cause);
-    }
+    log.warn(formatClass(origin) + join(msgs), cause);
   }
 
   @SafeVarargs
   public static <T, V> void warn(boolean gameInfo, Class<T> origin, V... msgs) {
-    if (enabled) {
-      log.warn(formatClass(origin) + join(msgs) + (gameInfo ? getGameInfo() : ""));
-    }
+    log.warn(formatClass(origin) + join(msgs) + (gameInfo ? getGameInfo() : ""));
   }
 
   @SafeVarargs
   public static <T, V> void debug(Class<T> origin, V... msgs) {
-    if (enabled) {
-      log.debug(formatClass(origin) + join(msgs));
-    }
+    log.info("Debug is: " + log.isDebugEnabled());
+    log.debug(formatClass(origin) + join(msgs));
   }
 
   @SafeVarargs
   public static <T, V> void debug(Class<T> origin, Throwable cause, V... msgs) {
-    if (enabled) {
-      log.debug(formatClass(origin) + join(msgs), cause);
-    }
+    log.debug(formatClass(origin) + join(msgs), cause);
   }
 
   @SafeVarargs
   public static <T, V> void debug(boolean gameInfo, Class<T> origin, V... msgs) {
-    if (enabled) {
-      log.debug(formatClass(origin) + join(msgs) + (gameInfo ? getGameInfo() : ""));
-    }
+    log.debug(formatClass(origin) + join(msgs) + (gameInfo ? getGameInfo() : ""));
   }
 
   @SafeVarargs
   public static <T, V> void error(Class<T> origin, V... msgs) {
-    if (enabled) {
-      log.error(formatClass(origin) + join(msgs));
-    }
+    log.error(formatClass(origin) + join(msgs));
   }
 
   @SafeVarargs
   public static <T, V> void error(Class<T> origin, Throwable cause, V... msgs) {
-    if (enabled) {
-      log.error(formatClass(origin) + join(msgs), cause);
-    }
+    log.error(formatClass(origin) + join(msgs), cause);
   }
 
   @SafeVarargs
   public static <T, V> void error(boolean gameInfo, Class<T> origin, V... msgs) {
-    if (enabled) {
-      log.error(formatClass(origin) + join(msgs) + (gameInfo ? getGameInfo() : ""));
-    }
+    log.error(formatClass(origin) + join(msgs) + (gameInfo ? getGameInfo() : ""));
   }
 
 }
