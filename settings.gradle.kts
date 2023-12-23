e1079af5d831390979cd9e08279607faab2e63d1
Fesaa <77553571+Fesaa@users.noreply.github.com>
2023-12-24 00:44:09 +0100

    Fireball fix

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
-rootProject.name = "Cubepanion"
+rootProject.name = "cubepanion"
 
 pluginManagement {
-    val labyGradlePluginVersion = "0.3.28"
+    val labyGradlePluginVersion = "0.3.43"
     plugins {
         id("net.labymod.gradle") version (labyGradlePluginVersion)
     }
