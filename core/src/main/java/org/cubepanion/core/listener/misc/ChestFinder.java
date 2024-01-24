b385db0f70794096eb5036f5acac2b2d50f25c21
Fesaa <77553571+Fesaa@users.noreply.github.com>
2024-01-24 21:36:19 +0100

    - Fix chest firing when setting is turned off

--- core/src/main/java/org/cubepanion/core/listener/misc/ChestFinder.java
+++ core/src/main/java/org/cubepanion/core/listener/misc/ChestFinder.java
@@ -5,6 +5,7 @@ import net.labymod.api.event.Subscribe;
 import net.labymod.api.event.client.chat.ChatReceiveEvent;
 import net.labymod.api.util.concurrent.task.Task;
 import org.cubepanion.core.Cubepanion;
+import org.cubepanion.core.config.QOLConfig;
 import org.cubepanion.core.utils.CubeGame;
 import org.cubepanion.core.versionlinkers.ChestFinderLink;
 import org.jetbrains.annotations.NotNull;
@@ -12,8 +13,11 @@ import org.jetbrains.annotations.NotNull;
 public class ChestFinder {
 
   private final Task task;
+  private final QOLConfig config;
 
   public ChestFinder(Cubepanion addon, @NotNull ChestFinderLink link) {
+    config = addon.configuration().getQolConfig();
+
     task = Task.builder(() -> {
       if (addon.getManager().getDivision().equals(CubeGame.LOBBY)) {
         link.displayLocations();
@@ -23,6 +27,9 @@ public class ChestFinder {
 
   @Subscribe
   public void onChatMessage(ChatReceiveEvent e) {
+    if (!config.getChestLocation().get()) {
+      return;
+    }
     String chestMessage = "A chest has been hidden somewhere in the Lobby with some goodies inside!";
     if (e.chatMessage().getPlainText().equals(chestMessage)) {
       task.execute();
