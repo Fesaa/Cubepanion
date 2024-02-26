1c7af43046e9387e5e5c8da71830fd799e27c7db
Amelia <77553571+Fesaa@users.noreply.github.com>
2024-02-26 18:22:46 +0100

    Fix script

--- generate_proto.sh
+++ generate_proto.sh
@@ -1,1 +1,3 @@
-curl https://raw.githubusercontent.com/Fesaa/CubepanionAPI/master/packets.proto | protoc I=./ --java_out=./ -
+curl https://raw.githubusercontent.com/Fesaa/CubepanionAPI/master/packets.proto >> packets.proto
+protoc -I=./ --java_out=./core/src/main/java packets.proto
+rm packets.proto
