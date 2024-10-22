fbc61afdc171386c9645e01fede1229350a1aaba
Amelia <77553571+Fesaa@users.noreply.github.com>
2024-10-23 00:17:19 +0200

    1.21.2 support

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "cubepanion"
 
 pluginManagement {
-    val labyGradlePluginVersion = "0.5.5"
+    val labyGradlePluginVersion = "0.5.6"
     plugins {
         id("net.labymod.gradle") version (labyGradlePluginVersion)
     }
